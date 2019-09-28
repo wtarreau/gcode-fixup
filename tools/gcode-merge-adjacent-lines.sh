e1945251723cf2e4fd65e642118e6013b24cdd8a
Willy Tarreau <w@1wt.eu>
2019-09-28 19:42:41 +0200

    tools/gcode-merge-adjacent-lines: also merge adjacent G0 commands
    Some B/W files can have adjacent G0 lines, so let's merge them too.
    Some files shrink by 20% with this.

--- tools/gcode-merge-adjacent-lines.sh
+++ tools/gcode-merge-adjacent-lines.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# merge G1 commands which only move X with no strength change
-exec awk 'BEGIN{line="";}/^G1 X[-0-9. ]*$/{line=$0;next;}{ if (line != "") print line; line=""; print $0;} END{ print line;}' "$@"
+# merge G0 or G1 commands which only move X with no strength change
+awk 'BEGIN{line="";}/^G1 X[-0-9. ]*$/{line=$0;next;}{ if (line != "") print line; line=""; print $0;} END{ if (line != "") print line;}' "$@" | \
+  awk 'BEGIN{line="";}/^G0 X[-0-9. ]*$/{line=$0;next;}{ if (line != "") print line; line=""; print $0;} END{ if (line != "") print line;}'
