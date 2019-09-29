d52010962649971f2fbb9f5ccbc1e7c926ccb36e
Willy Tarreau <w@1wt.eu>
2019-09-29 22:41:51 +0200

    src: laser-preview: add a function to draw a line
    The function tries to mark beam spot once per pixel and to center them
    around the middle of a visited segment. Then it calls the burn() function
    which tries to mark up to 4 pixels depending on its position. For now the
    burn() function only marks one, as the written calculations are wrong.

--- src/laser-preview.c
+++ src/laser-preview.c
@@ -148,6 +148,119 @@ int extend_img(struct img *img, int nx0, int ny0, int nx1, int ny1)
 	return 1;
 }
 
+/* mark the 1x1 area around (x,y) as burnt, taking the intensity and overlap
+ * into account. There can be up to 4 pixels affected.
+ */
+static inline int burn(struct img *img, float x, float y, float intensity)
+{
+	int x0, y0, x1, y1;
+	float s00, s01, s10, s11; // fraction of overlapping surface
+
+	x0 = (int)(x - 0.5); x1 = (int)(x + 0.5);
+	y0 = (int)(y - 0.5); y1 = (int)(y + 0.5);
+
+	/* FIXME, still wrong */
+	//s00 = (x0 - x + 0.5) * (y0 - y + 0.5);
+	//s01 = (x0 - x + 0.5) * (y + 0.5 - y1);
+	//s10 = (x + 0.5 - x1) * (y0 - y + 0.5);
+	//s11 = (x + 0.5 - x1) * (y + 0.5 - y1);
+
+	/* FIXME: wrong values, and test for the need to extend */
+	img->area[(y0 - img->y0) * (img->x1 - img->x0 + 1) + (x0 - img->x0)] += intensity;
+
+	/* FIXME: also pass feed speed to compute time spent on location */
+
+	/* next steps: count energy delivered by the beam as intensity * time * ratio * absorption */
+	/* Don't forget that absorption is a function of the current energy of the cell since it
+	 * can change color.
+	 *
+	 * Then we have diffusion to surrounding pixels, which is a function of their distance
+	 * and depends on the material. Long dispersion means the energy is exchanged to other
+	 * places thus there is less locally and more remotely. Short dispersion means we only
+	 * burn under the beam. Ideally the dispersion should take the time into account so that
+	 * we can apply it with the feed speed, allowing the local spot to cool down and not
+	 * reach a burning temperature.
+	 */
+	return 1;
+}
+
+/* Draw a vector in <img> from (x0,y0) to (x1,y1) included at intensity
+ * <intensity>. The principle consists in cutting the vector into 1-px large
+ * steps (vert or horiz) and assigning the beam energy in the middle of each
+ * segment, with the principle that the beam is 1x1 px centered in the middle
+ * of a pixel around (0.5, 0.5), where all the energy is sent. The area under
+ * the beam will get its share of the energy depending on how it overlaps.
+ *
+ * Thus when asking to draw a segment between pixels (0,0) and (2,1), a 2-pixel
+ * segment will be drawn between locations (0.5,0.5) and (2.5,1.5), split like
+ * this:
+ *   1st segment: (0.5,0.5)-(1.5,1.0)
+ *   2nd segment: (1.5,1.0)-(2.5,1.5)
+ * resulting in the following locations to be burnt:
+ *   - 1x1 around (1.0,0.75) = (0.5,0.25)-(1.5,1.25)
+ *   - 1x1 around (2.0,1.25) = (1.5,0.75)-(2.5,1.75)
+ *
+ * Returns non-zero if OK, 0 on error.
+ *
+ * Example, with movement from location F (0,0) to T (4,2):
+ *
+ *     0   1   2   3   4      1st step: move from (0,0) to (1,0.5)
+ *   +---+---+---+---+---+     = beam center from (0.5,0.5) to (1.5,1.0)
+ * 2 |   |   |   |   | T |    -> beam spot (1.0, 0.75) -> (0.5,0.25)-(1.5,1.25)
+ *   +---+---+---+-/-+---+    +---+---+
+ * 1 |   |   | / |   |   |    | .---. | < 1.75
+ *   +---+-/-+---+---+---+    +-|-+-|-+
+ * 0 | F |   |   |   |   |    | '---' | < 0.75
+ *   +---+---+---+---+---+    +---+---+
+ *
+ */
+int draw_vector(struct img *img, int x0, int y0, int x1, int y1, float intensity)
+{
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+
+	if (!dx && !dy)
+		return 1;
+
+	if (abs(dx) >= abs(dy)) {
+		/* must visit all X places */
+		float x, y, ystep;
+
+		if (dx < 0) {
+			dx = -dx;
+			x0 = x1;
+			x1 = x0 + dx;
+		}
+		ystep = (float)dy / dx;
+
+		for (x = x0 + 0.5; (int)x < x1 ; x += 1.0) {
+			/* aim the beam at (x,y) */
+			y = y0 + 0.5 + ystep * (x - x0 + 0.5 /* for mid-trip */);
+			/* So beam overlaps with (x-0.5,y-0.5,x+0.5,y+0.5) */
+			if (!burn(img, x, y, intensity))
+				return 0;
+		}
+	} else {
+		/* must visit all Y places */
+		float x, y, xstep;
+
+		if (dy < 0) {
+			dy = -dy;
+			y0 = y1;
+			y1 = y0 + dy;
+		}
+		xstep = (float)dx / dy;
+
+		for (y = y0 + 0.5; (int)y < y1 ; y += 1.0) {
+			/* aim the beam at (x, y+0.5) */
+			x = x0 + 0.5 + xstep * (y - y0 + 0.5 /* for mid-trip */);
+			/* So beam overlaps with (x-0.5,y-0.5,x+0.5,y+0.5) */
+			if (!burn(img, x, y, intensity))
+				return 0;
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	uint8_t *buffer;
@@ -183,6 +296,10 @@ int main(int argc, char **argv)
 			img.area[y * w + x] = 0.5 * y / h + 0.5 * x / w;
 		}
 	}
+
+	draw_vector(&img, 300,300,500,600, 255);
+	draw_vector(&img, 300,300,600,600, 255);
+	draw_vector(&img, 300,300,600,500, 255);
 
 	for (y = 0; y < h; y++) {
 		for (x = 0; x < w; x++) {
